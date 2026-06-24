bef8169f0c44c6bb713569c30d843a061b0970c4
Debasmita-Rana <debasmitarana04@gmail.com>
2026-06-24 15:21:36 +0530

    E-Learning Platform

---
+++ Backend/config/db.js
@@ -0,0 +1,12 @@
+const  mongoose = require('mongoose');
+const connectdb = async ()=>{
+    try{
+        await mongoose.connect(process.env.dburl);
+        console.log("mongodb connected");
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
+}
+module.exports=connectdb;
